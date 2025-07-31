3108f45781acf27f62f54caeda68d0a5c6fb74f7
Martin McCarthy <ma310770@ucf.edu>
2025-07-30 22:23:00 -0400

    audio updates

--- Assets/CollisionDetection.cs
+++ Assets/CollisionDetection.cs
@@ -6,6 +6,9 @@ public class CollisionDetection : MonoBehaviour
 {
     [SerializeField] private string m_locationName = "";
     [SerializeField] private GameObject m_manipulateObject;
+    [SerializeField] private AudioSource m_audioSource;
+    [SerializeField] private AudioClip m_clipToPlay;
+
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.transform.CompareTag("Vehicle"))
@@ -15,9 +18,24 @@ public class CollisionDetection : MonoBehaviour
                 case "Library":
                     m_manipulateObject.SetActive(false);
                     break;
+                case "Lenore1":
+                    PlayAudio();
+                    break;
+                case "Lenore2":
+                    PlayAudio();
+                    break;
+                case "Raven":
+                    PlayAudio();
+                    break;
                 default:
                     break;
             }
         }
+    }
+
+    private void PlayAudio()
+    {
+        m_audioSource.clip = m_clipToPlay;
+        m_audioSource.Play();
     }
 }
